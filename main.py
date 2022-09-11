38902d6bf5cc6ebedf9d317cb34e43011d66096f
hadib <hadibeydoun8@hotmail.com>
2022-09-11 11:10:37 -0400

    fix // for api url

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import APIHandler
 import time
 
 token = '1bf756322d822760d3153f9f54be7bfe1cea94a6'
-api_url = 'http:\\192.168.86.34:2550/api'
+api_url = 'http://192.168.86.34:2550/api'
 config_path = '\\config'
 
 f = APIHandler.APIHandler(token, api_url, config_path)
